96762301d84dd9029620d1410af7e1baffe2e5b1
jemshMarcel <jemshjms@gmail.com>
2025-04-27 16:13:20 +0300

    Create post model

---
+++ app/Models/Post.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'slug',
+        'thumbnail',
+        'body',
+        'active',
+        'published_at',
+        'user_id'
+    ];
+
+    public function user():BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function categories():BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
