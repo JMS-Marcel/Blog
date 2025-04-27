4a81d6aa668ba143f25b072ede425370bf051931
jemshMarcel <jemshjms@gmail.com>
2025-04-27 19:53:30 +0300

    update post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Post extends Model
 {
@@ -25,8 +26,8 @@ class Post extends Model
         return $this->belongsTo(User::class);
     }
 
-    public function categories():BelongsTo
+    public function categories():BelongsToMany
     {
-        return $this->belongsTo(Category::class);
+        return $this->belongsToMany(Category::class);
     }
 }
